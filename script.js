d6f910b46e95053e7f88e5d712af2f50eb8a5f7a
Tyler Hevia <tylerhevia@gmail.com>
2017-10-17 22:24:41 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const tab1 = $('.tab1');
+const tab2 = $('.tab2');
+const tab3 = $('.tab3');
+const tab4 = $('.tab4');
+
+const showCodeButton = $('.show-code-button');
+
+showCodeButton.on('click', () => {
+  console.log('fart');
+});
